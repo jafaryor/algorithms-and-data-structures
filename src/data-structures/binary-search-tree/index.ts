78bbffe67683d8cc475bb815b112899f9d117028
Jafar Yormahmadzoda <yormahmadzoda@google.com>
2020-03-03 15:50:30 -0800

    BST insert

--- src/data-structures/binary-search-tree/index.ts
+++ src/data-structures/binary-search-tree/index.ts
@@ -1,39 +1,62 @@
 /**
  * The Binary Search Tree.
+ * [ left < root >= right ]
  * The height of the binary tree is h = log_2(n)
  */
 export class BinarySearchTree<T> {
   root: BinaryNode<T>;
+  inorderWalkLogs: number[] = [];
 
   constructor(key: number, value: T) {
     this.root = new BinaryNode<T>(key, value);
   }
 
   /**
-   * Prints the tree from left to right (from min to max).
+   * Prints the tree in the following pattern: [ left | parent | right ]
+   * Prints the left -> root -> right
    * @complexity O(n)
    */
-  inorderTreeWalk(node?: BinaryNode<T>): void {
+  inorderTreeWalk(node?: BinaryNode<T> = this.root): void {
     if (!node) return;
 
     this.inorderTreeWalk(node.left);
+
     console.log(node.value);
+    // logging logic - start.
+    if (node === this.root) this.inorderWalkLogs = [];
+    this.inorderWalkLogs.push(node.key);
+    // end.
+
     this.inorderTreeWalk(node.right);
   }
 
   /**
-   * Prints the tree from root to leaves,
-   * starting from the specified node.
+   * Prints the tree in the following pattern: [ parent | left | right ]
+   * Prints children -> root
    * @complexity O(n)
    */
-  preorderTreeWalk(node?: BinaryNode<T>): void {}
+  preorderTreeWalk(node?: BinaryNode<T>): void {
+    if (!node) return;
+
+    console.log(node.value);
+
+    this.preorderTreeWalk(node.left);
+    this.preorderTreeWalk(node.right);
+  }
 
   /**
-   * Prints the tree from leaves to root,
-   * starting from the specified node.
+   * Prints the tree in the following pattern: [ left | right | parent ]
+   * Prints root -> children
    * @complexity O(n)
    */
-  postorderTreeWalk(node?: BinaryNode<T>): void {}
+  postorderTreeWalk(node?: BinaryNode<T>): void {
+    if (!node) return;
+
+    this.postorderTreeWalk(node.left);
+    this.postorderTreeWalk(node.right);
+
+    console.log(node.value);
+  }
 
   /**
    * Recursively searches for a value in the binary tree starting from root.
@@ -100,8 +123,6 @@ export class BinarySearchTree<T> {
    * Returns the pointer to the successor of the node.
    * The successor of a node A is a node with the smallest key,
    * grater than A.key.
-   * Basically if you order all tree values from min to max,
-   * the successor is the item located from the right side of the item.
    * @complexity O(h)
    */
   successor(node: BinaryNode<T>): BinaryNode<T>|undefined {
@@ -125,7 +146,67 @@ export class BinarySearchTree<T> {
    * @complexity O(h)
    */
   predecessor(node: BinaryNode<T>): BinaryNode<T>|undefined {
-    return node;
+    if (node.left) return this.max(node.left);
+
+    let a = node;
+    let b = node.parent;
+
+    while (b && a === b.left) {
+      a = b;
+      b = b.parent;
+    }
+
+    return b;
+  }
+
+  /**
+   * Inserts a new node into the tree. (Recursive approach)
+   * @complexity O(h)
+   */
+  insert(newNode: BinaryNode<T>, node: BinaryNode<T> = this.root): void {
+    if (!this.root) {
+      this.root = newNode;
+    }
+
+    if (newNode.key < node.key) {
+      if (node.left)
+        return this.insert(newNode, node.left);
+      else {
+        newNode.parent = node;
+        node.left = newNode;
+      }
+    } else {
+      if (node.right)
+        return this.insert(newNode, node.right);
+      else {
+        newNode.parent = node;
+        node.right = newNode;
+      }
+    }
+  }
+
+  /**
+   * Inserts a new node into the tree. (Iterative approach)
+   * @complexity O(h)
+   */
+  iterativeInsert(newNode: BinaryNode<T>, node: BinaryNode<T> = this.root):
+      void {
+    let a: BinaryNode<T>|undefined = node;
+    let b: BinaryNode<T>;
+
+    while (a) {
+      b = a;
+      a = newNode.key < a.key ? a.left : a.right;
+    }
+
+    newNode.parent = b;
+
+    if (!b)
+      this.root = newNode;
+    else if (newNode.key < b.key)
+      b.left = newNode;
+    else
+      b.right = newNode;
   }
 }
 
