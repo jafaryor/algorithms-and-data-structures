59678d28b57ca4dcac56ebf11967e32a44dd2907
Jafar Yormahmadzoda <yormahmadzoda@google.com>
2020-03-13 15:54:36 -0700

    rbt insert

--- src/data-structures/red-black-tree/index.ts
+++ src/data-structures/red-black-tree/index.ts
@@ -1,12 +1,16 @@
+import {swap} from '../../utils';
 import {BinarySearchTree} from '../binary-search-tree';
-import {RedBlackNode} from './node';
+import {RedBlackColorEnum as RedBlackNodeColor, RedBlackNode} from './node';
 
 /**
  * The Red-Black Tree implmenetation.
  */
-export class RedBlackTree<T> extends BinarySearchTree {
+export class RedBlackTree<T> extends BinarySearchTree<T> {
+    root: RedBlackNode<T> | undefined;
+
     constructor() {
         super();
+        this.root = undefined;
     }
 
     /**
@@ -64,4 +68,104 @@ export class RedBlackTree<T> extends BinarySearchTree {
         left.right = node;
         node.parent = left;
     }
+
+    /**
+     * Recursive insert.
+     * @complexity O(lg n)
+     */
+    insert(newNode: RedBlackNode<T>) {
+        this.super.insert(newNode);
+
+        newNode.color = RedBlackNodeColor.RED;
+
+        this.insertFixup(newNode);
+    }
+
+    /**
+     * Iterative insert.
+     * @complexity O(lg n)
+     */
+    iterativeInsert(newNode: RedBlackNode<T>) {
+        this.super.iterativeInsert(newNode);
+
+        newNode.color = RedBlackNodeColor.RED;
+
+        this.insertFixup(newNode);
+    }
+
+    /**
+     * Restores the reb-black tree properties after insertion.
+     */
+    insertFixup(newNode: RedBlackNode<T>) {
+        let node = newNode as RedBlackNode<T>;
+        let parent: RedBlackNode<T>;
+        let grandParent: RedBlackNode<T>;
+        let uncle: RedBlackNode<T>;
+
+        while (node !== this.root
+            && node.color !== RedBlackNodeColor.BLACK
+            && node.parent.color === RedBlackNodeColor.RED
+        ) {
+            parent = node.parent;
+            grandParent = parent.parent;
+
+            // Case A: parent is left child of grandparent.
+            if (grandParent.left === parent) {
+                uncle = grandParent.right;
+
+                // Case 1: The uncle is RED.
+                if (uncle && uncle.color === RedBlackNodeColor.RED) {
+                    // Repaint.
+                    grandParent.color = RedBlackNodeColor.RED;
+                    parent.color = RedBlackNodeColor.BLACK;
+                    uncle.color = RedBlackNodeColor.BLACK;
+                    node = grandParent;
+                } else {
+                    // Case 2: node is right child.
+                    if (parent.right === node) {
+                        // Rotate left.
+                        this.leftRotate(parent);
+                        node = parent;
+                        parent = node.parent;
+                    }
+
+                    // Case 3: node is left child.
+                    // Rotate right.
+                    this.rightRotate(grandParent);
+                    swap(parent.color, grandParent.color);
+                    node = parent;
+                }
+            } else {
+                // Case B: Parent is right child of grandparent.
+                uncle = grandParent.left;
+
+                // Case 1: uncle is also RED.
+                if (uncle && uncle.color === RedBlackNodeColor.RED) {
+                    // Repaint.
+                    grandParent.color = RedBlackNodeColor.RED;
+                    parent.color = RedBlackNodeColor.BLACK;
+                    uncle.color = RedBlackNodeColor.BLACK;
+                    node = grandParent;
+                } else {
+                    // Case 2: node is left child.
+                    if (uncle && uncle.color === RedBlackNodeColor.RED) {
+                        // Rotate right.
+                        this.rightRotate(parent);
+                        node = parent;
+                        parent = node.parent;
+                    }
+
+                    // Case 3: node is right child.
+                    // Rotate left.
+                    this.leftRotate(grandParent);
+                    swap(parent.color, grandParent.color);
+                    node = parent;
+                }
+            }
+        }
+
+        this.root.color = RedBlackNodeColor.BLACK;
+    }
 }
+
+// https://www.geeksforgeeks.org/c-program-red-black-tree-insertion/
