26629ae4b7a07c58e2079dafbdd7637e0c03869e
Jafar Yormahmadzoda <jafaryor@gmail.com>
2022-01-15 21:51:12 -0800

    linted

--- src/problems/arithmetic-expression-evaluation.ts
+++ src/problems/arithmetic-expression-evaluation.ts
@@ -1,4 +1,4 @@
-import { Stack } from "..";
+import {Stack} from '..';
 
 /**
  * Dijkstra's Two-Stack Algorithm for Arithmetic Expression Evaluation.
@@ -20,48 +20,52 @@ export function evaluateArithmeticExpression(expression: string): number {
     let value: number;
     const operators = new Stack<string>();
     const values = new Stack<number>();
-    const inputs = expression.split(" ") as string[];
+    const inputs = expression.split(' ') as string[];
 
     for (const input of inputs) {
         // Ignore left parenthesis.
-        if (input === "(") continue;
+        if (input === '(') continue;
         else if (isOperator(input)) operators.push(input);
-        else if (input === ")") {
+        else if (input === ')') {
             operator = operators.pop()!;
             value = values.pop()!;
 
             // Apply the math operator.
             switch (operator) {
-                case "+":
+                case '+':
                     value = values.pop()! + value;
                     break;
-                case "-":
+                case '-':
                     value = values.pop()! - value;
                     break;
-                case "*":
+                case '*':
                     value = values.pop()! * value;
                     break;
-                case "/":
+                case '/':
                     value = values.pop()! / value;
                     break;
-                case "sqrt":
+                case 'sqrt':
                     value = Math.sqrt(value);
                     break;
             }
 
             values.push(value);
-        }
-        else values.push(Number(input));
+        } else values.push(Number(input));
     }
 
     return values.pop()!;
 }
-
 
 /**
  * Checks if the input is an arithmetic operator.
  * @complexity O(1)
  */
 function isOperator(input: string): boolean {
-    return input === "+" || input === "-" || input === "*" || input === "/" || input === "sqrt";
+    return (
+        input === '+' ||
+        input === '-' ||
+        input === '*' ||
+        input === '/' ||
+        input === 'sqrt'
+    );
 }
--- src/problems/tests/arithmetic-expression-evaluation.spec.ts
+++ src/problems/tests/arithmetic-expression-evaluation.spec.ts
@@ -1,15 +1,21 @@
-import { evaluateArithmeticExpression } from "../arithmetic-expression-evaluation";
+import {evaluateArithmeticExpression} from '../arithmetic-expression-evaluation';
 
 describe('Evaluation of Math Expression', () => {
-    it ('Case 01', () => {
-        expect(evaluateArithmeticExpression('( 1 + ( ( 2 + 3 ) * ( 4 * 5 ) ) )')).toEqual(101);
+    it('Case 01', () => {
+        expect(
+            evaluateArithmeticExpression('( 1 + ( ( 2 + 3 ) * ( 4 * 5 ) ) )'),
+        ).toEqual(101);
     });
 
     it('Case 02', () => {
-        expect(evaluateArithmeticExpression('( ( 2 + sqrt ( 4 ) ) / 2 )')).toEqual(2);
+        expect(
+            evaluateArithmeticExpression('( ( 2 + sqrt ( 4 ) ) / 2 )'),
+        ).toEqual(2);
     });
 
     it('Case 03', () => {
-        expect(evaluateArithmeticExpression('( ( 20 - sqrt ( 16 ) ) * 1 )')).toEqual(16);
+        expect(
+            evaluateArithmeticExpression('( ( 20 - sqrt ( 16 ) ) * 1 )'),
+        ).toEqual(16);
     });
 });
